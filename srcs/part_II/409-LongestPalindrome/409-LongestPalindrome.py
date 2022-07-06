807ba79c3836c6cd800ed2c0de13eb45549f6607
Natali <kunata928@gmail.com>
2022-07-06 17:10:50 +0400

    412

--- srcs/part_II/409-LongestPalindrome/409-LongestPalindrome.py
+++ srcs/part_II/409-LongestPalindrome/409-LongestPalindrome.py
@@ -14,6 +14,18 @@ class Solution:
                 max_odd = 1
         return res + max_odd
 
+    def longestPalindrome1(self, s):
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
+            else:
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
+
 
 strs = [
 "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
@@ -23,6 +35,6 @@ strs = [
     # "aaaabbbccd"
 ]
 for s in strs:
-    print(s, Solution.longestPalindrome(Solution, s))
+    print(s, Solution.longestPalindrome1(Solution, s))
 
 #len include all even letters and len odd letter - 1 (and extra 1, if odd exist)
---
+++ srcs/part_II/412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,30 @@
+class Solution:
+    def fizzBuzz(self, n):
+        res = [str(i) for i in range(1, n+1)]
+        for i in range(2, n, 3):
+            res[i] = "Fizz"
+        for i in range(4, n, 5):
+            res[i] = "Buzz"
+        for i in range(14, n, 15):
+            res[i] = "FizzBuzz"
+        return res
+
+    def fizzBuzz1(self, n):
+        res = list()
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0:
+                res.append("Fizz")
+            elif i % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+        return res
+
+    def fizzBuzz2(self, n):
+        # list comprehension with string rule of FizzBuzz
+        list_of_output = ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n + 1)]
+        return list_of_output
+
+print(Solution.fizzBuzz1(Solution, 16))
